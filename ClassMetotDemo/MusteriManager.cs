7fe8659d7a1c344ba58db56d71c679cd8549db6c
Kerem Demiralay <68017152+BudderFortress7@users.noreply.github.com>
2021-01-23 00:07:22 +0300

    Add files via upload

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri eklendi " + musteri.Ad + " " + musteri.Soyad);
+;       }
+        public void Kaldır(Musteri musteri)
+        {
+            Console.WriteLine("Musteri kaldırıldı "+musteri.Ad+" "+musteri.Soyad);
+        }
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (var i in musteriler)
+            {
+                Console.WriteLine(i.Ad);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriManager yardımcı = new MusteriManager();
+            Musteri musteri1 = new Musteri();
+            Musteri musteri2 = new Musteri();
+            Musteri musteri3 = new Musteri();
+            Musteri musteri4 = new Musteri();
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3, musteri4 };
+            musteri1.Ad = "Kerem";
+            musteri1.Id = 684;
+            musteri1.Soyad = "Demiralay";
+            musteri1.Ad = "Kerem";
+            yardımcı.Ekle(musteri1);
+            yardımcı.Kaldır(musteri1);
+            yardımcı.Listele(musteriler);
+            Console.ReadLine();
+        }
+    }
+}
