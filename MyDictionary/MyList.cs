a5b81b8b7916c96b49c2636234e5131fd6d150a4
Kerem Demiralay <68017152+BudderFortress7@users.noreply.github.com>
2021-01-24 11:56:59 +0300

    Add files via upload

---
+++ MyDictionary/MyList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyList<T>
+    {
+        T[] items;
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T veri)
+        {
+            T[] geciciyer = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < geciciyer.Length; i++)
+            {
+                items[i] = geciciyer[i];
+            }
+            items[items.Length - 1] = veri;
+
+        }
+        public T[] Items
+        {
+            get
+            {
+                return items;
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Kerem");
+            isimler.Add("Ömer");
+            foreach (var itm in isimler.Items)
+            {
+                Console.WriteLine(itm);
+            }
+            MyList<int> sayilar = new MyList<int>();
+            sayilar.Add(1343);
+            sayilar.Add(021354);
+            foreach (int sad in sayilar.Items)
+            {
+                Console.WriteLine(sad);
+            }
+            Console.ReadLine();
+        }
+    }
+}
